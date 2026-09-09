649844dfac5da470255e97c48f4e6af4159316a9
Luccas Santos Barbosa <luccassantosbarbosa@gmail.com>
2026-09-08 21:24:04 -0300

    Add primary key to cd_produto in tb_Produtos table

--- db_RegistroProdutos.sql
+++ db_RegistroProdutos.sql
@@ -2,7 +2,7 @@ CREATE DATABASE db_RegistroProdutos;
 USE db_RegistroProdutos;
 
 CREATE TABLE tb_Produtos(
-cd_produto int NOT NULL auto_increment,
+cd_produto int NOT NULL auto_increment primary key,
 nm_produto varchar(80),
 ds_produto varchar(180),
 qtd_estoque int,
